b164c439af804242fc1b56ef412054515ac85a9b
James Lloyd <james.robert.lloyd@gmail.com>
2012-12-20 18:06:06 +0000

    Updated sandpit to use new package

--- source/sandpit.py
+++ source/sandpit.py
@@ -4,7 +4,8 @@ Created on 23 Nov 2012
 @author: James Lloyd jrl44@cam.ac.uk
 '''
 
-import pyfear
+#import pyfear
+import cblparallel
 import time
 from sklearn.cross_validation import cross_val_score
 from sklearn.datasets import make_blobs
@@ -87,6 +88,27 @@ print "Goodbye, world"
 quit()
 '''
 
+reduce_tree_code = '''
+import pickle
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import subprocess
+from subprocess_timeout import timeoutCommand
+# Load data
+print 'Loading data'
+with open('%(data_file)s', 'r') as f:
+    (X_train, y_train, X_test) = pickle.load(f)
+print 'Loaded data'
+# Train some trees
+estimator = RandomForestRegressor(n_estimators=%(n_trees)d, max_depth=None, min_samples_split=1, random_state=%(random_state)d).fit(X_train, y_train)
+print 'Trained trees'
+# Predict
+prediction = estimator.predict(X_test)
+'Predicted'
+# Output
+np.savetxt('%(output_file)s', prediction, delimiter=',')
+'''
+
 def mkstemp_safe(directory, suffix):
     (os_file_handle, file_name) = tempfile.mkstemp(dir=directory, suffix=suffix)
     os.close(os_file_handle)
@@ -115,6 +137,52 @@ def rf_fear_test(n=10,n_trees=1000):
         fear.copy_to(data_file, os.path.join(remote_temp_path, os.path.split(data_file)[-1]))
         output_files = pyfear.run_python_jobs(scripts, local_temp_path, remote_temp_path, fear)
         fear.rm(os.path.join(remote_temp_path, os.path.split(data_file)[-1]))
+
+    # Kill local data file
+    os.remove(data_file)    
+
+    # Now do something with the output
+
+    estimators = []
+    predictions = []
+
+    for output_file in output_files:
+        with open(output_file, 'r') as f:
+            #(estimator, prediction) = pickle.load(f)
+            prediction = np.genfromtxt(output_file, delimiter=',')
+        os.remove(output_file)
+        #estimators.append(estimator)
+        predictions.append(prediction)
+
+    #ens = EnsembleRegressor(estimators)
+    #return RMSE(X_test, y_test, ens)
+
+    ens_pred = np.mean(predictions, axis=0)
+    return RMSE_y(y_test, ens_pred)
+    
+def rf_fear_test2(n=10,n_trees=10):
+    # Data
+    X, y = make_friedman1(n_samples=1200, random_state=0, noise=1.0)
+    X_train, X_test = X[:200], X[200:]
+    y_train, y_test = y[:200], y[200:]
+    # Params
+#    local_temp_path = os.path.abspath('../temp/')
+#    remote_temp_path = 'python/'
+    # Write data file locally
+    data_file = mkstemp_safe(local_temp_path, '.p')
+    with open(data_file, 'w') as f:
+        pickle.dump((X_train, y_train, X_test), f)
+    # Prepare code
+    scripts = [tree_code % {'data_file' : os.path.split(data_file)[-1],
+                            'n_trees' : n_trees,
+                            'random_state' : i * n_trees,
+                            'output_file' : '%(output_file)s',
+                            'flag_file' : '%(flag_file)s'} for i in range(n)]
+    # Submit to fear
+    with cblparallel.fear() as fear:
+        fear.copy_to(data_file, os.path.join(cblparallel.config.REMOTE_TEMP_PATH, os.path.split(data_file)[-1]))
+        output_files = pyfear.run_python_jobs(scripts)
+        fear.rm(os.path.join(cblparallel.config.REMOTE_TEMP_PATH, os.path.split(data_file)[-1]))
 
     # Kill local data file
     os.remove(data_file)    
